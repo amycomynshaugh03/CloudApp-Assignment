5ecddb57c83bfb4f5c767289987329b4b9c56482
Amy Comyns Haugh <comynsamy03@gmail.com>
2026-03-04 16:22:29 +0000

    Initial CDK TypeScript Project Structure imported

---
+++ lib/ca-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CaQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
